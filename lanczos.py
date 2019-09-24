2729dced811ea4e9c9d340c2ffbba5cb5b467726
Lukas Murmann <lmurmann@mit.edu>
2019-09-24 16:21:50 -0400

    Add support for mip-generation - partial light probe support

---
+++ lanczos.py
@@ -0,0 +1,164 @@
+"""Lanczos filter implementation (uint8, uint16, float32, float64).
+
+Evaluated alternatives:
+  PIL has a lanczos filter but only for uint8.
+  scipy.mis.imresize() silently converts float to uint8 and calls into PIL
+  skimage only has lanczos upsampling but downsampling uses gaussian filter.
+
+The implementation below is reasonably efficient thanks to scipy sparse matrix
+multiply. For better performance this can be replaced with  a specialized lanczos
+implementation such as Intel Integrated Performance Primitives.
+"""
+
+import numpy as np
+import argparse
+import math
+from math import floor, ceil
+
+import scipy
+from scipy.sparse import csr_matrix
+
+
+__all__ = ('resize_lanczos')
+
+def resize_lanczos(I, h, w):
+  I = horizontal_lanczos(I, w)
+  # we force the intermediate result after x-filter back into I.dtype.
+  # this might lead to different roundoff error than an implementation that
+  # stores the intermediate result as floats.
+  I = np.swapaxes(I, 0, 1)
+  I = horizontal_lanczos(I, h)
+  return np.swapaxes(I, 0, 1)
+
+
+def lanczos2(x):
+  """Evaluate the three-lobed (a=2) lanczos filter. The image filter
+  precomputes filter weights once per image and reuses the same weights
+  for each image row (horizontal filter) or column (vertical filter)."""
+  a = 2
+  ret = np.zeros_like(x)
+  ret[x == 0] = 1
+  ret[x != 0] = np.sinc(x[x != 0]) * np.sinc(x[x != 0] / a)
+  ret[np.abs(x) >= a] = 0
+  return ret
+
+def saturation_cast(I, dtype):
+  """Utility for filtering integer images"""
+  if dtype=='uint8':
+    return np.clip(I, 0, 255).astype(dtype)
+  elif dtype=='uint16':
+    return np.clip(I, 0, 65535).astype(dtype)
+  elif dtype in ['float32', 'float64']:
+    return I.astype(dtype)
+  else:
+    raise ValueError("Saturation cast for type %s not implemented" % I.type)
+
+
+def horizontal_lanczos(I, w, comment=None):
+  """Apply 3-lobed Lanczos filter along horizontal (x) dimension.
+
+  Returned image has shape (I.shape[0], w, I.shape[2]) and same
+  dtype as the input image.
+
+  Set "comment" argument for debug information
+  """
+  dt = I.dtype
+  inh, inw = I.shape[:2]
+  nchan = 1 if len(I.shape) == 2 else I.shape[2]
+  a = 2
+  winsz = 5
+  if comment:
+    from lum.tictoc import tictoc
+    print("winsz is", winsz)
+
+
+  if comment:
+    print("compute %s filter" % comment)
+    tic = tictoc()
+
+  xs = inw/w
+  xfltscale = max(1, xs)
+  srange = np.arange(floor(-2*xfltscale), ceil(2*xfltscale)+1)
+
+  # We treat image filtering as sparse linear matrix multiply R = M * A
+  # with filter matrix M. We start by constructing the matrix.
+  ntap = len(srange)
+  row = np.zeros([w,ntap], dtype='int32')
+  col = np.zeros([w,ntap], dtype='int32')
+  data = np.zeros([w,ntap], dtype='float32')
+
+  if comment:
+    print("using %d tap filter" % len(srange))
+
+  for x in range(w):
+    center = x * xs
+    r = (center + srange).round().astype('int32')
+    weights = lanczos2((r - center) / xfltscale)
+
+    # handle image bounds, normalize filter weight
+    # in order to handle out of bounds, we clamp the indices
+    # to [0, w) and set the associated weight to zero.
+    weights[r<0] = 0
+    weights[r>=inw] = 0
+    r[r<0] = 0
+    r[r>=inw] = 0
+    weights /= np.sum(weights)
+
+    for ti, c in enumerate(r):
+      row[x, ti] = x
+      col[x, ti] = c
+      data[x, ti] = weights[ti]
+
+  row = np.reshape(row, [-1])
+  col = np.reshape(col, [-1])
+  data = np.reshape(data, [-1])
+  M = csr_matrix((data, (row, col)), shape=(w, inw))
+
+  if comment:
+    print("took", tic.toc())
+    tic.tic()
+
+  if comment:
+    print("apply %s filter" % comment)
+
+  # Construct right-hand side. R = M * A
+  # Each column of A has one row of the input image.
+  # A has (I.ncols * I.nchans) columns
+  A = np.moveaxis(I, 1, 0)
+  A = np.reshape(A, [inw, inh*nchan])
+  R = (M * scipy.sparse.csr_matrix(A)).toarray()
+
+  # Reshape R into [h,w,c] image format
+  R = np.reshape(R, [w, inh, nchan])
+  R = np.moveaxis(R, 1, 0)
+  R = saturation_cast(R, dt)
+  R = R.reshape([inh, w, nchan])
+
+  if comment:
+    print("took", tic.toc())
+  return R
+
+def main():
+  """Test stub for development only"""
+  from multilum import readexr, writeexr
+  from lum import plt
+  from lum import pfm
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("image")
+  opts = parser.parse_args()
+
+  if opts.image.endswith("jpg"):
+    I = pfm.load_jpg3(opts.image, dtype="uint8")
+  else:
+    I = readexr(opts.image)
+
+  writeexr(I, "outfsz.exr")
+  Ism = resize_lanczos(I, 100, 150)
+  writeexr(Ism, "out.exr")
+
+  plt.gshow(Ism)
+  plt.show()
+
+if __name__ == "__main__":
+  main()
--- multilum.py
+++ multilum.py
@@ -6,6 +6,8 @@ import zipfile
 import tqdm
 import numpy as np
 
+from lanczos import resize_lanczos
+
 # PIL and OpenEXR are imported lazily if needed
 
 ############################## DEV
@@ -45,24 +47,87 @@ def readexr(path):
   w, h = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
 
   rgb = [np.ndarray([h,w], dtype="float32", buffer=fh.channel(c)) for c in ['R', 'G', 'B']]
-  ret = np.zeros([1000, 1500, 3], dtype='float32')
+  ret = np.zeros([h, w, 3], dtype='float32')
   for i in [0,1,2]:
     ret[:,:,i] = rgb[i]
   return ret
 
+def writeimage(I, path):
+  if path.endswith("exr"):
+    return writeexr(I, path)
+  elif path.endswith("jpg"):
+    return writejpg(I, path)
+  else:
+    raise ValueError("Unrecognized file type for path %s" % path)
+
+def writejpg(I, path):
+  from PIL import Image
+  im = Image.fromarray(I)
+  im.save(path, "JPEG", quality=95)
+
+def writeexr(I, path):
+  import OpenEXR
+  import Imath
+  h, w = I.shape[:2]
+  head = OpenEXR.Header(w, h)
+  head["compression"] = Imath.Compression(Imath.Compression.DWAB_COMPRESSION)
+  of = OpenEXR.OutputFile(path, head)
+  R, G, B = [I[:,:,c].tobytes() for c in [0,1,2]]
+  of.writePixels({'R': R, 'G': G, 'B': B})
+
+
 def impath(scene, dir, mip, hdr):
   return os.path.join(basedir, name(scene), "dir_%d_mip%d.%s" % (dir, mip, ext(hdr)))
 
+def imshape(mip):
+  return 4000 // 2 **mip, 6000 // 2 **mip
+
+
+def probepath(scene, dir, material, size, hdr):
+  return os.path.join(basedir, name(scene), "probes", "dir_%d_%s%d.%s" % (dir, material, size, ext(hdr)))
+
 
 # ____________ Per-scene functions ____________
+
+FRONTAL_DIRECTIONS = [2, 3, 19, 20, 21, 22, 24]
+NOFRONTAL_DIRECTIONS = [i for i in range(25) if i not in FRONTAL_DIRECTIONS]
+
+def has_larger_version(scene, mip, hdr):
+  return get_larger_version(scene, mip, hdr) != -1
+
+def get_larger_version(scene, mip, hdr):
+  for testmip in range(mip-1, -1, -1):
+    if scene_is_downloaded(scene, testmip, hdr):
+      return testmip
+  return -1
+
+def generate_mipmap(scene, mip, hdr):
+  print("generating mipmap %d for scene %s/hdr=%d" % (mip, scene, hdr))
+  srcmip = get_larger_version(scene, mip, hdr)
+  if srcmip == -1:
+    raise ValueError("Cannot generate mip level %d for scene %s" % (mip, scene))
+
+  outh, outw = imshape(mip)
+  for dir in range(25):
+    I = readimage(impath(scene, dir, srcmip, hdr))
+    I = resize_lanczos(I, outh, outw)
+    writeimage(I, impath(scene, dir, mip, hdr))
+
+
+
 def scene_is_downloaded(scene, mip, hdr):
   testfile = impath(scene, 0, mip, hdr)
+  return os.path.isfile(testfile)
+
+def probe_is_downloaded(scene, material, size, hdr):
+  testfile = probepath(scene, 0, material, size, hdr)
+  # print("testing file", testfile)
   return os.path.isfile(testfile)
 
 def download_scenes(scenes=None, *, mip=2, hdr=False, force=False):
   def download_scene(scene):
     fmt = "exr" if hdr else "jpg"
-    url = BASE_URL + "/%s_mip2_%s.zip" % (scene, fmt)
+    url = BASE_URL + "/%s_mip%d_%s.zip" % (scene, mip, fmt)
     req = urllib.request.urlopen(url)
     archive = zipfile.ZipFile(io.BytesIO(req.read()))
     archive.extractall(basedir)
@@ -76,8 +141,19 @@ def download_scenes(scenes=None, *, mip=2, hdr=False, force=False):
       download_scene(scene)
 
 def ensure_downloaded(scene, mip, hdr):
+  # from pdb import set_trace as st
+  # st()
   if not scene_is_downloaded(scene, mip, hdr):
-    download_scenes([scene], mip=mip, hdr=hdr)
+    if has_larger_version(scene, mip, hdr):
+      generate_mipmap(scene, mip, hdr)
+    else:
+
+      download_scenes([scene], mip=mip, hdr=hdr)
+
+def ensure_probe_downloaded(scene, material, size, hdr):
+  if not probe_is_downloaded(scene, material, size, hdr):
+    raise ValueError("Do not have %s probe (size=%d, hdr=%d) for scene %s" % (material, size, hdr, scene))
+
 
 def all_scenes():
   pass
@@ -121,10 +197,7 @@ def query_images(scenes=None, dirs=None, *, mip=2, hdr=False):
   scenes = sanitize_scenes_arg(scenes)
   dirs = sanitize_dirs_arg(dirs)
 
-
-  h = 4000 // 2 **mip
-  w = 6000 // 2 **mip
-
+  h, w = imshape(mip)
   ret = np.zeros([len(scenes), len(dirs), h, w, 3], dtype=dtype(hdr))
 
   i = 0
@@ -134,23 +207,46 @@ def query_images(scenes=None, dirs=None, *, mip=2, hdr=False):
       ret[iscene, idir] = readimage(impath(scene, dir, mip, hdr))
   return ret
 
+def query_probes(scenes=None, dirs=None, material="chrome", *, size=256, hdr=False):
+  scenes = sanitize_scenes_arg(scenes)
+  dirs = sanitize_dirs_arg(dirs)
 
+
+  h, w = size, size
+
+  ret = np.zeros([len(scenes), len(dirs), h, w, 3], dtype=dtype(hdr))
+
+  i = 0
+  for iscene, scene in enumerate(scenes):
+    for idir, dir in enumerate(dirs):
+      ensure_probe_downloaded(scene, material=material, size=size, hdr=hdr)
+      ret[iscene, idir] = readimage(probepath(scene, dir, material, size, hdr))
+  return ret
 
 # ____________ MAIN STUB FOR DEVELOPMENT ____________
 
 
 def main():
-  scene_names = ["main_admin1", "summer_bathroom8"]
-  hdr=False
-  download_scenes(scene_names, hdr=hdr)
+  from lum import plt
+  mip=2
+  # scene_names = ["main_admin1", "summer_bathroom8"]
+  hdr=True
+  # download_scenes(scene_names, hdr=hdr)
 
   scene_names = ["main_admin1", "summer_bathroom8", "summer_bathroom7"]
 
-  I = query_images(scene_names, hdr=hdr)
-  print(I.shape)
-  for scene in I:
-    for dir in scene:
-      plt.imshow(dir)
+  # download_scenes(scene_names, mip=mip, hdr=hdr, force=True)
+
+  I = query_images(scene_names, dirs=NOFRONTAL_DIRECTIONS, hdr=hdr, mip=mip)
+  P = query_probes(scene_names, dirs=NOFRONTAL_DIRECTIONS, hdr=hdr)
+  print("I shape", I.shape)
+  print("P shape", P.shape)
+  for scene, probe in zip(I, P):
+    for imdir, probedir in zip(scene, probe):
+      plt.subplot(121)
+      plt.cshow(imdir)
+      plt.subplot(122)
+      plt.cshow(probedir)
       plt.show()
       break
 
